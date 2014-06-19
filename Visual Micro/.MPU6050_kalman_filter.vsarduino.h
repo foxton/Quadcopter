1bcd47f893dbe242be58e6d3a88630b1df3b45be
foxton <foxx89@hotmail.com>
2014-06-19 21:37:13 +0200

    Quadcopter

--- Visual Micro/.MPU6050_kalman_filter.vsarduino.h
+++ Visual Micro/.MPU6050_kalman_filter.vsarduino.h
@@ -1,12 +1,11 @@
 /* 
 	Editor: http://www.visualmicro.com
-	        arduino debugger, visual micro +, free forum and wiki
-	        both visualmicro and the arduino ide ignore this code during compilation. this code is automatically maintained by visualmicro, manual changes to this file will be overwritten
+	        visual micro and the arduino ide ignore this code during compilation. this code is automatically maintained by visualmicro, manual changes to this file will be overwritten
 	        the contents of the Visual Micro sketch sub folder can be deleted prior to publishing a project
 	        all non-arduino files created by visual micro and all visual studio project or solution files can be freely deleted and are not required to compile a sketch (do not delete your own code!).
 	        note: debugger breakpoints are stored in '.sln' or '.asln' files, knowledge of last uploaded breakpoints is stored in the upload.vmps.xml file. Both files are required to continue a previous debug session without needing to compile and upload again
 	
-	Hardware: Arduino Uno, Platform=avr, Package=arduino
+	Hardware: Arduino Nano w/ ATmega328, Platform=avr, Package=arduino
 */
 
 #ifndef _VSARDUINO_H_
@@ -45,13 +44,17 @@ extern "C" void __cxa_pure_virtual() {;}
 //
 void idle();
 void gyro();
+void StartESC();
 uint8_t i2cWrite(uint8_t registerAddress, uint8_t data, bool sendStop);
 uint8_t i2cWrite(uint8_t registerAddress, uint8_t *data, uint8_t length, bool sendStop);
 uint8_t i2cRead(uint8_t registerAddress, uint8_t *data, uint8_t nbytes);
 
 #include "C:\Program Files (x86)\Arduino\hardware\arduino\avr\cores\arduino\arduino.h"
-#include "C:\Program Files (x86)\Arduino\hardware\arduino\avr\variants\standard\pins_arduino.h" 
-#include "G:\Arduino Projects\Quadcopter\MPU6050 Gyroscope\MPU6050_kalman_filter\MPU6050_kalman_filter.ino"
-#include "G:\Arduino Projects\Quadcopter\MPU6050 Gyroscope\MPU6050_kalman_filter\I2C.ino"
-#include "G:\Arduino Projects\Quadcopter\MPU6050 Gyroscope\MPU6050_kalman_filter\Kalman.h"
+#include "C:\Program Files (x86)\Arduino\hardware\arduino\avr\variants\eightanaloginputs\pins_arduino.h" 
+#include "C:\Users\Johansson\Downloads\Dennis\Quadcopter\MPU6050 Gyroscope\MPU6050_kalman_filter\MPU6050_kalman_filter.ino"
+#include "C:\Users\Johansson\Downloads\Dennis\Quadcopter\MPU6050 Gyroscope\MPU6050_kalman_filter\I2C.ino"
+#include "C:\Users\Johansson\Downloads\Dennis\Quadcopter\MPU6050 Gyroscope\MPU6050_kalman_filter\Kalman.h"
+#include "C:\Users\Johansson\Downloads\Dennis\Quadcopter\MPU6050 Gyroscope\MPU6050_kalman_filter\PID_v1.h"
+#include "C:\Users\Johansson\Downloads\Dennis\Quadcopter\MPU6050 Gyroscope\MPU6050_kalman_filter\Servo.h"
+#include "C:\Users\Johansson\Downloads\Dennis\Quadcopter\MPU6050 Gyroscope\MPU6050_kalman_filter\resource.h"
 #endif
